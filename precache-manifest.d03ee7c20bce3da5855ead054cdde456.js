8862cef6ac0d65295142d55d8d4a97bf58562fac
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2020-01-13 17:30:39 +0530

    Updates

---
+++ precache-manifest.d03ee7c20bce3da5855ead054cdde456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649466ebf56974f63264eb76fa6349d0",
+    "url": "/my-ways/index.html"
+  },
+  {
+    "revision": "256022d3d2db379ac3f8",
+    "url": "/my-ways/static/css/main.7b2fdd1e.chunk.css"
+  },
+  {
+    "revision": "67e9d2831e6669eb8826",
+    "url": "/my-ways/static/js/2.42463183.chunk.js"
+  },
+  {
+    "revision": "1cf4077b186339249e244f8aa69b4e3e",
+    "url": "/my-ways/static/js/2.42463183.chunk.js.LICENSE"
+  },
+  {
+    "revision": "256022d3d2db379ac3f8",
+    "url": "/my-ways/static/js/main.c347f56f.chunk.js"
+  },
+  {
+    "revision": "f02634722c8de6165cdc",
+    "url": "/my-ways/static/js/runtime-main.c9b6d5bd.js"
+  }
+]);
